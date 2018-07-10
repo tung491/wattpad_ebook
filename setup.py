ce18303a4daf13bdac8af475f50365af8a6277d1
tung491 <dosontung007@gmail.com>
2018-07-11 00:59:11 +0700

    fix install-requires

--- setup.py
+++ setup.py
@@ -7,17 +7,19 @@ requisites = []
 
 setup(
     name='wattpad_ebook',
-    version='0.0.1',
-    description='Generating ebook from wattpad',
+    version='0.0.7',
+    description='Generate ebook from Wattpad, '
+                'upload into Google Drive and send to Kindle',
     author='Do Son Tung',
+    url='https://github.com/tung491/wattpad_ebook',
     author_email='dosontung007@gmail.com',
-    packages='wattpad_ebook',
+    packages=['wattpad_ebook'],
     license='MIT',
+    install_requires=['requests_html'],
 
-
-    entry_points = {
-                   'console_scripts': [
-                       'wattpad_ebook=wattpad_ebook.wattpad_ebook:cli',
-                   ],
-               },
+    entry_points={
+        'console_scripts': [
+            'wattpad-ebook=wattpad_ebook.wattpad_ebook:cli'
+        ],
+    }
 )
